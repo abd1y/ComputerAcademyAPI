2cd1cc8a7b041457f35503888935ba0cb4fedc26
Rossevelt <Abduillh160@gmail.com>
2026-05-08 15:51:13 +0300

    user setting

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,8 @@
+
+
+
 from django.urls import  path
-from .views import SingIn,Longin,Reset_password,For_get_password_st1,For_get_password_st2,For_get_password_st3,Updit_Profile,Profile_user
+from .views import SingIn,Longin,Reset_password,For_get_password_st1,For_get_password_st2,For_get_password_st3,Updit_Profile,Profile_user,UserSetting
 
 
 urlpatterns = [
@@ -11,6 +14,7 @@ urlpatterns = [
     path("For-get-password_Step2/",For_get_password_st2,name='For_get_password_st2'),
     path("For-get-password_Step3/",For_get_password_st3,name='For_get_password_st2'),
     path("Updit-profile/",Updit_Profile,name="Updit_Profile"),
+    path("UserSetting/",UserSetting),
     path("Profile_user/<int:id>/",Profile_user,name="Updit_Profile"),
     
 
